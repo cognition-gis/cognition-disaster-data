977940a5c423ced961d708dfdbc8deccdbe814f8
Loaner Laptop <geospatialjeff@gmail.com>
2019-06-06 16:19:54 -0700

    adding band mappings for appropriate DG sensors

---
+++ disaster_data/sources/dg_open_data/band_mappings.py
@@ -0,0 +1,90 @@
+
+WV04 = [
+        {
+            "name": "blue",
+            "center_wavelength": 480
+        },
+        {
+            "name": "green",
+            "center_wavelength": 545
+        },
+        {
+            "name": "red",
+            "center_wavelength": 672.5
+        },
+        {
+            "name": "nir",
+            "center_wavelength": 850
+        },
+        {
+            "name": "pan",
+            "center_wavelength": 625
+        }
+    ]
+
+WV03 = [
+        {
+            "name": "blue",
+            "center_wavelength": 480
+        },
+        {
+            "name": "green",
+            "center_wavelength": 545
+        },
+        {
+            "name": "red",
+            "center_wavelength": 660
+        },
+        {
+            "name": "nir",
+            "center_wavelength": 832.5
+        }
+    ]
+
+
+WV02 = [
+        {
+            "name": "blue",
+            "center_wavelength": 478
+        },
+        {
+            "name": "green",
+            "center_wavelength": 546
+        },
+        {
+            "name": "red",
+            "center_wavelength": 659
+        },
+        {
+            "name": "nir",
+            "center_wavelength": 831
+        }
+    ]
+
+
+WV01 = [
+        {
+            "name": "pan",
+            "center_wavelength": 650
+        }
+    ]
+
+
+GE01 = [
+        {
+            "name": "blue",
+            "center_wavelength": 480
+        },
+        {
+            "name": "green",
+            "center_wavelength": 550
+        },
+        {
+            "name": "red",
+            "center_wavelength": 672.5
+        },
+        {
+            "name": "nir",
+            "center_wavelength": 850
+        }
+    ]
