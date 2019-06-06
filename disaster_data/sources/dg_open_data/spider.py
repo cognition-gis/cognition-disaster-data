fd490711c19cac9654af4ff96a2edc0e1adb1eec
Loaner Laptop <geospatialjeff@gmail.com>
2019-06-06 16:15:42 -0700

    adding method to pull partial stac items from dg imagery tables

--- disaster_data/sources/dg_open_data/spider.py
+++ disaster_data/sources/dg_open_data/spider.py
@@ -1,7 +1,24 @@
 import scrapy
 from scrapy.crawler import CrawlerProcess
 
-from disaster_data.sources.dg_open_data.utils import items_from_imagery_table
+def items_from_imagery_table(table):
+    out_list = []
+    for row in table.xpath('.//tbody/tr'):
+        date = row.xpath('.//td/p/text()').get()
+        assets = [x for x in row.xpath('.//td/a/@href').getall() if x.endswith('.tif')]
+        for asset in assets:
+            partial_item = {
+                'properties': {
+                    'datetime': date
+                },
+                'assets': {
+                    'data': {
+                        'href': asset
+                    }
+                }
+            }
+            out_list.append(partial_item)
+    return out_list
 
 class DGOpenDataCollections(scrapy.Spider):
     name = 'dg-open-data'
