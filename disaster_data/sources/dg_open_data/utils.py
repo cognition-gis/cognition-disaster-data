d0b91fd0f3221cf60296d579e45ccee00348064b
Jeff Albrecht <geospatialjeff@gmail.com>
2019-07-10 21:39:47 -0700

    integrating with stac-updater and backfilling collection extent

--- disaster_data/sources/dg_open_data/utils.py
+++ disaster_data/sources/dg_open_data/utils.py
@@ -1,25 +1,31 @@
 import os
 import json
-import tempfile
-import uuid
 import math
 from datetime import datetime
 from multiprocessing import Process, Pipe
 import subprocess
 import itertools
 
+import boto3
 import requests
 from satstac import Collection, Item
 
 from disaster_data.scraping import ScrapyRunner
-from disaster_data.sources.dg_open_data.spider import DGOpenDataCollections
+from disaster_data.sources.dg_open_data.spider import DGOpenDataCatalog, DGOpenDataSummary
 from . import band_mappings
-
 
 from osgeo import gdal
 
+lambda_client = boto3.client('lambda')
+s3_client = boto3.client('s3')
+
 root_url = 'https://cognition-disaster-data.s3.amazonaws.com'
 oam_upload_url = 'https://api.openaerialmap.org/uploads'
+thumbnail_bucket = 'cognition-disaster-data'
+thumbnail_key_prefix = 'thumbnails'
+thumbnail_kickoff_bucket = 'cognition-thumbnails-kickoff'
+stac_updater_arn = 'arn:aws:lambda:us-east-1:725820063953:function:stac-updater-dev-kickoff'
+
 oam_cookie = os.environ['OAM_COOKIE']
 dg_api_key = os.environ['DG_API_KEY']
 
@@ -48,7 +54,6 @@ def append_dg_metadata(stac_item):
         "content-type": "application/x-www-form-urlencoded",
         'x-api-key': dg_api_key,
     }
-    # stac_item = stac_payload['item']
     imgid = stac_item['assets']['data']['href'].split('/')[-2]
 
     xvals = [x[0] for x in stac_item['geometry']['coordinates'][0]]
@@ -83,14 +88,6 @@ def append_dg_metadata(stac_item):
                 dg_props.update({f"dg:{k}": v})
         stac_item['properties'].update(dg_props)
 
-        # Add browse url to assets
-        stac_item['assets'].update({
-            'thumbnail': {
-                'href': stac_item['properties'].pop('dg:browse_url'),
-                'type': 'image/jpeg'
-            }
-        })
-
         # Add band mappings
         band_count = stac_item['properties'].pop('bandcount')
         stac_item['properties'].update({'eo:bands': getattr(band_mappings, stac_item['properties']['eo:platform'])})
@@ -108,9 +105,8 @@ def append_dg_metadata(stac_item):
         stac_item['properties']['dg:collect_time_start'] = collect_time_start
         stac_item['properties']['dg:collect_time_end'] = collect_time_end
         stac_item['properties']['datetime'] = collect_time_start
-
-        return stac_item
     else:
+        print("Bad query on DG image {}_{}".format(imgid, stac_item['id']))
         # Parse necessary information from filename
         # Return as partial item (without DG metadata appended)
         splits = stac_item['assets']['data']['href'].split('/')
@@ -118,10 +114,27 @@ def append_dg_metadata(stac_item):
             'dg:legacy_identifier_reference': splits[-2],
             'datetime': splits[-3],
         })
-        return stac_item
+
+    # Generate a unique thumbnail URL (backfill later with aws batch)
+    thumbnail_key = os.path.join(thumbnail_key_prefix,
+                                 stac_item['collection'],
+                                 stac_item['properties']['datetime'].split('T')[0],
+                                 stac_item['properties']['dg:legacy_identifier_reference'],
+                                 stac_item['id'] + '.jpg')
+    stac_item['assets'].update({
+        'thumbnail': {
+            'href': 'https://{}.s3.amazonaws.com/{}'.format(thumbnail_bucket, thumbnail_key),
+            'type': 'image/jpeg'
+        }
+    })
+
+    return stac_item
 
 def append_gdal_info(partial_item):
-    file_url = os.path.join("/vsicurl/" + partial_item['assets']['data']['href'])
+    try:
+        file_url = os.path.join("/vsicurl/" + partial_item['assets']['data']['href'])
+    except KeyError:
+        print("Bad item", partial_item)
     info = gdal.Info(file_url, format='json', allMetadata=True)
 
     # Calculating geometry and bbox
@@ -135,23 +148,31 @@ def append_gdal_info(partial_item):
         filename = '/'.join(filename.split('/')[2:])
 
     partial_item.update({
-        'id': os.path.splitext(filename)[0].split('/')[-1],
         'bbox': [min(xvals), min(yvals), max(xvals), max(yvals)],
         'geometry': {
             'type': 'Polygon',
             'coordinates': geometry
-        },
-        'properties': {
-            'eo:epsg': info['coordinateSystem']['wkt'].rsplit('"EPSG","', 1)[-1].split('"')[0],
-            'eo:gsd': (info['geoTransform'][1] + abs(info['geoTransform'][-1])) / 2,
-            'bandcount': len(info['bands'])
-        }})
+        }
+    })
+    partial_item['properties'].update({
+        'eo:epsg': info['coordinateSystem']['wkt'].rsplit('"EPSG","', 1)[-1].split('"')[0],
+        'eo:gsd': (info['geoTransform'][1] + abs(info['geoTransform'][-1])) / 2,
+        'bandcount': len(info['bands'])
+    })
 
 def _complete_stac_item(partial_stac_items, conn):
 
     for partial_item in partial_stac_items:
+        # Append metadata to stac item with GDAL and DG Browse API
         append_gdal_info(partial_item)
         append_dg_metadata(partial_item)
+
+        # Add to stac catalog with stac-updater
+        lambda_client.invoke(
+            FunctionName=stac_updater_arn,
+            InvocationType="Event",
+            Payload=json.dumps(partial_item)
+        )
 
     conn.send(partial_stac_items)
     conn.close()
@@ -166,16 +187,21 @@ def complete_stac_items(partial_stac_items, batch_size, num_threads):
         process = Process(target=_complete_stac_item, args=(list(itertools.islice(partial_stac_items, batch_size)), child_conn))
         processes.append(process)
 
+    print("Starting processes")
     for process in processes:
         process.start()
 
+    print("Joining processes")
     for process in processes:
         process.join()
 
+    print("Getting results from processes")
+    extent_list = []
     for parent_connection in parent_connections:
         resp = parent_connection.recv()
         for item in resp:
-            yield item
+            extent_list.append({'bbox': item['bbox'], 'collection': item['collection']})
+    return extent_list
 
 def create_collections(collections):
     dg_collection = Collection.open(os.path.join(root_url, 'DGOpenData', 'catalog.json'))
@@ -186,10 +212,12 @@ def create_collections(collections):
     out_d = {}
     for coll in collections:
         if coll['id'] not in current_cat_names:
+            print("Creating new collection: {}".format(coll['id']))
             new_coll = Collection(coll)
             dg_collection.add_catalog(new_coll)
             out_d.update({coll['id']: new_coll})
         else:
+            print("Opening existing collection: {}".format(coll['id']))
             out_d.update({coll['id']:Collection.open(os.path.join(root_url, 'DGOpenData', coll['id'], 'catalog.json'))})
 
     dg_collection.save()
@@ -244,9 +272,9 @@ def oam_upload(cookie, payload):
     resp = subprocess.call(f'curl -H "cookie: {cookie}" -H "Content-Type: application/json" -d @{payload} https://api.openaerialmap.org/uploads', shell=True)
     return resp
 
-def build_dg_catalog(id_list, num_threads=10, limit=None, stac=True, oam=False):
+def build_dg_catalog(id_list, num_threads=10, limit=None, collections_only=False):
 
-    with ScrapyRunner(DGOpenDataCollections) as runner:
+    with ScrapyRunner(DGOpenDataCatalog) as runner:
         partial_items = runner.execute(ids=id_list, items=True)
         collections = next(partial_items)
         item_count = next(partial_items)
@@ -255,19 +283,28 @@ def build_dg_catalog(id_list, num_threads=10, limit=None, stac=True, oam=False):
             item_count = limit
 
         batch_size = int(math.ceil(item_count / num_threads))
+        print("Item count: {}".format(item_count))
+        print("Batch size: {}".format(batch_size))
+
+        # Build and ingest stac collections
         open_collections = create_collections(collections)
-        complete_items = complete_stac_items(partial_items, batch_size, num_threads)
 
-        create_stac_items(complete_items, open_collections)
+        if collections_only:
+            return
 
-        #
-        # if oam:
-        #     # Map STAC items to OAM items
-        #     oam_scenes = stac_to_oam(complete_items)
-        #     # Create payload and upload to OAM
-        #     tempdir = tempfile.mkdtemp()
-        #     temp_fpath = os.path.join(tempdir, str(uuid.uuid4()))
-        #     with open(temp_fpath, 'w') as outfile:
-        #         json.dump(oam_scenes, outfile, indent=2)
-        #
-        #     oam_upload(oam_cookie, temp_fpath)
+        # Build and ingest stac items
+        item_extents = complete_stac_items(partial_items, batch_size, num_threads)
+
+        print("Finished building STAC items.")
+        for id in id_list:
+            xmin_vals = [x['bbox'][0] for x in item_extents if x['collection'] == id]
+            xmax_vals = [x['bbox'][2] for x in item_extents if x['collection'] == id]
+            ymin_vals = [x['bbox'][1] for x in item_extents if x['collection'] == id]
+            ymax_vals = [x['bbox'][3] for x in item_extents if x['collection'] == id]
+
+            collection_extent = [min(xmin_vals), min(ymin_vals), max(xmax_vals), max(ymax_vals)]
+            print("Calculated extent of {} collection: {}".format(id, collection_extent))
+
+            # Backfill extent with sat-stac
+            open_collections[id].data.update({'extent': collection_extent})
+            open_collections[id].save()
