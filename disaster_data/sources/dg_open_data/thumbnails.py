33684026a274c7880ed5fdab98c59b60fb6f0695
Jeff Albrecht <geospatialjeff@gmail.com>
2019-07-15 12:21:11 -0700

    adding method to rebuild thumbnails for a specific sensor

---
+++ disaster_data/sources/dg_open_data/thumbnails.py
@@ -0,0 +1,24 @@
+import os
+import json
+
+import boto3
+from satstac import Collection
+
+root_url = 'https://cognition-disaster-data.s3.amazonaws.com'
+
+sqs = boto3.client('sqs')
+account_name = boto3.client('sts').get_caller_identity().get('Account')
+
+def find_sensor(collection_name, sensor_name):
+    col = Collection.open(os.path.join(root_url, 'DGOpenData', collection_name, 'catalog.json'))
+    for item in col.items():
+        if 'eo:platform' in item.properties:
+            if item.properties['eo:platform'] == sensor_name:
+                yield item
+
+def rebuild_thumbnails(collection_name, sensor_name):
+    for item in find_sensor(collection_name, sensor_name):
+        sqs.send_message(
+            QueueUrl=f'https://sqs.us-east-1.amazonaws.com/{account_name}/newThumbnailQueue',
+            MessageBody=json.dumps(item.data)
+        )
